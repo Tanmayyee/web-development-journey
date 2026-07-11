4c84ab41ab611514bc318443173026cbe1be5857
Tanmay Johri <jo.tanmay7@gmail.com>
2026-07-12 00:07:00 +0530

    Add input and change event

---
+++ JAVASCRIPT/02-DOM_and_Events/09-input_and_change_events.js
@@ -0,0 +1,23 @@
+const input=document.querySelector('input')
+const h1=document.querySelector('h1')
+const h2=document.querySelector('h2')
+const input2=document.querySelector('#z')
+const head2=document.querySelector('#head2')
+
+input.addEventListener('input',function(e){           //input event: Triggered whenever the user changes the input value
+    h2.innerText=input.value
+})
+
+input2.addEventListener('change',function(e){       //change event : runs when the input value is changed and confirmed  or input loses focus
+    head2.innerText=input2.value
+})
+
+
+
+//other events ::
+'click'    // when an element is clicked
+'input'    // when the input value changes (live change will show)
+'change'   // when the value changes and the input loses focus (for text inputs)  ( hit enter or click somewhere else to confirm input)
+'submit'   // when a form is submitted
+'keydown'  // when a key is pressed
+'keyup'    // when a key is released
