b98349b918fda6fab7b0b546174ba0e6fd11ca0f
Tanmay Johri <jo.tanmay7@gmail.com>
2026-07-12 20:57:18 +0530

    Add select and reset button logic

--- JAVASCRIPT/03-JS_Mini_Projects/04-Score_keeper.js
+++ JAVASCRIPT/03-JS_Mini_Projects/04-Score_keeper.js
@@ -1,6 +1,7 @@
 const p1Btn=document.querySelector('#p1Btn')
 const p2Btn=document.querySelector('#p2Btn')
 const resetBtn=document.querySelector('#resetBtn')
+const select=document.querySelector('#select')
 
 let p1Score=0;
 let p2Score=0;
@@ -28,3 +29,19 @@ p2Btn.addEventListener('click',function(){
         isGameOver=true; 
     }
 })
+
+select.addEventListener('change',function(){
+    winningScore=parseInt(select.value)                     //select.value will give string , so parseInt is used to change string into number ,,, example "7" convert into 7
+                                                        //or - winningScore=parseInt(this.value) ,, .this respresent select
+   reset()                                           
+})
+
+resetBtn.addEventListener('click',reset)
+
+function reset(){
+    p1Score=0;
+    p2Score=0;
+    p2Display.textContent=0;
+    p1Display.textContent=0;
+    isGameOver=false;
+}
