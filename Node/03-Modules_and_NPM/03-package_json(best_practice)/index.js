11413bb6fec367f09c770b93ec02570f297484e7
Tanmay Johri <jo.tanmay7@gmail.com>
2026-07-18 18:44:14 +0530

    Add package json(best practice)

---
+++ Node/03-Modules_and_NPM/03-package_json(best_practice)/index.js
@@ -0,0 +1,9 @@
+//best practice-------
+
+// npm init   --- to add package.json first
+// package name: package.json
+// npm i package-name    (this will install package and automatically add dependencies in package.json file for that package)
+
+const colors=require("colors")
+
+console.log(colors.rainbow('OMG Rainbows!!!! hehehehehehe'));
