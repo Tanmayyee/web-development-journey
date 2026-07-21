45fe0fa02c4807ac9e1045c512d965d65a9f3e97
Tanmay Johri <jo.tanmay7@gmail.com>
2026-07-21 17:50:40 +0530

    Pass dynamic data to random and subreddit EJS templates

--- Express/02-EJS/index.js
+++ Express/02-EJS/index.js
@@ -16,6 +16,21 @@ app.get("/", (req, res) => {
     res.render("home");
 });
 
+
+///ran route: Generates a random number between 1 and 10 and passes it to the random.ejs view.
+app.get('/ran',(req,res)=>{
+  const num=Math.floor(Math.random()*10+1)
+  res.render('random',{bubbles:num})
+  //or res.render('random',{num})
+})
+
+
+///r/:subreddit route: Extracts the dynamic subreddit name from the URL path and passes it to the subreddit.ejs view.
+app.get('/r/:subreddit',(req,res)=>{
+  const {subreddit}=req.params
+  res.render('subreddit',{subreddit})
+})
+
 app.listen(2000, () => {
   console.log("listning on port 2000");
 });
