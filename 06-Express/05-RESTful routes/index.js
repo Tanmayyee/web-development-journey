4cf5948dc4a63dfe34a8d9a28b5b233d49606d93
Tanmay Johri <jo.tanmay7@gmail.com>
2026-07-25 22:06:50 +0530

    Add uuid v4

--- 06-Express/05-RESTful routes/index.js
+++ 06-Express/05-RESTful routes/index.js
@@ -1,6 +1,7 @@
 import express, { urlencoded } from "express"
 const app=express()
 import path from "path"
+import { v4 as uuidv4 } from 'uuid';
 
 app.use(express.urlencoded({extended:true}))
 
@@ -11,22 +12,22 @@ app.set('views',path.join(import.meta.dirname,'/views'))
 //our fake database: 
 let fakeData =[
     {
-            id: 1,
+            id: uuidv4(),
             username: 'Todd',
             comment: 'lol that is so funny!'
         },
         {
-            id: 2,
+            id: uuidv4(),
             username: 'Skyler',
             comment: 'I like to go birdwatching with my dog'
         },
         {
-            id: 3,
+            id: uuidv4(),
             username: 'Sk8erBoi',
             comment: 'Plz delete your account, Todd'
         },
         {
-            id: 4,
+            id: uuidv4(),
             username: 'onlysayswoof',
             comment: 'woof woof woof'
         }
@@ -51,7 +52,7 @@ app.get('/comments/new',(req,res)=>{
 // **********************************
 app.post('/comments',(req,res)=>{
     const { comment , username  }=req.body
-    fakeData.push({username, comment})
+    fakeData.push({username, comment, id:uuidv4()})
     res.redirect('/comments')
 })
 
@@ -60,7 +61,7 @@ app.post('/comments',(req,res)=>{
 // *******************************************
 app.get('/comments/:id',(req,res)=>{
     const {id}=req.params
-    const showComt=fakeData.find(c=> c.id===parseInt(id))       // /comments/:id  gives id in string form , therefore parseInt is used here.
+    const showComt=fakeData.find(c=> c.id===id)       // /comments/:id  gives id in string form , therefore parseInt is used here.
     res.render('show',{showComt})
 })
 
