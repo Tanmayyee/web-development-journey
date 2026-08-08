39aecad67878c91a56007950648121515eb6ac77
Tanmay Johri <jo.tanmay7@gmail.com>
2026-08-08 22:52:39 +0530

    add models folder

---
+++ 07-Database/02-Mongoose_with_Express/models/product.js
@@ -0,0 +1,3 @@
+import mongoose from "mongoose";
+
+//mongoose connection with mongoose not required here because we are importing this model in index.js where we have already connected to the database
