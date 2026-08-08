80a37cde681738fcebae85d8ebb8cc3284024009
Tanmay Johri <jo.tanmay7@gmail.com>
2026-08-08 22:28:58 +0530

    add more basic setup

--- 07-Database/02-Mongoose_with_Express/index.js
+++ 07-Database/02-Mongoose_with_Express/index.js
@@ -1,5 +1,16 @@
 import express from "express";
+const app=express()
+import path from "path"
 import mongoose from "mongoose";
 
 // console.log("testing npm init")
 
+app.set('views',path.join(import.meta.dirname,'/views'))
+app.set('view engine','ejs')
+
+
+
+
+app.listen(2300,()=>{
+    console.log("Listening on Port 2300!")
+})
