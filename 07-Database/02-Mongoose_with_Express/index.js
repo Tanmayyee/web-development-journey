b779fbe3352bd027a8b04390c9749e66b354b3d3
Tanmay Johri <jo.tanmay7@gmail.com>
2026-08-08 22:34:35 +0530

    connect mongoose

--- 07-Database/02-Mongoose_with_Express/index.js
+++ 07-Database/02-Mongoose_with_Express/index.js
@@ -3,12 +3,28 @@ const app=express()
 import path from "path"
 import mongoose from "mongoose";
 
+const MONGO_URI = "mongodb://127.0.0.1:27017/mongooseWithExpress";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(MONGO_URI);
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error(`Error connecting to MongoDB: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+await connectDB();
+
 // console.log("testing npm init")
 
 app.set('views',path.join(import.meta.dirname,'/views'))
 app.set('view engine','ejs')
 
-
+app.get('/dog',(req,res)=>{
+    res.send('WOOFFF!!!')
+})
 
 
 app.listen(2300,()=>{
