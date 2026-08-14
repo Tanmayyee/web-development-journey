e69c36677edcfe9827803507511d1597f6ea7e8b
Tanmay Johri <jo.tanmay7@gmail.com>
2026-08-14 15:20:39 +0530

    delete error handling file and create new error handling folder

--- 06-Express/07-Error-handling-in-express/01-Error-basics/errorhandling.js
+++ 06-Express/07-Error-handling-in-express/01-Error-basics/errorhandling.js
@@ -57,17 +57,6 @@ app.use((err,req,res,next)=>{
     res.status(status).send(message)
 })
 
-// throw new AppError("not allowed", 403)
-//                 ↓
-//         Express catches error
-//                 ↓
-//      skips normal middleware
-//                 ↓
-// app.use((err, req, res, next) => {
-//                 ↓
-//         err = AppError object
-// })
-
 app.listen(4000,()=>{
     console.log("port 4000 !!!")
 })
