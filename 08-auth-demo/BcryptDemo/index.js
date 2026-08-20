b77faf9875fee20b5689d6dc4c91fcf5246facc0
Tanmay Johri <jo.tanmay7@gmail.com>
2026-08-21 00:23:24 +0530

    feat(auth): add login comparison and simplify hash logic

--- 08-auth-demo/BcryptDemo/index.js
+++ 08-auth-demo/BcryptDemo/index.js
@@ -1,10 +1,18 @@
 import bcrypt, { hash } from 'bcrypt'
 
 const hashPassword= async(pw)=>{
-    const salt= await bcrypt.genSalt(12);
-    const hash= await bcrypt.hash(pw,salt)
-    console.log(salt);
-    console.log(hash)
+    const hash= await bcrypt.hash(pw,12)
+    console.log(hash);
 }
 
-hashPassword('helloimpassword')
+const login= async(pw,hashedpw)=>{
+    const result= await bcrypt.compare(pw,hashedpw);
+    if(result){
+        console.log("logged in!")
+    }else{
+        console.log("incorrect password!")
+    }
+}
+
+hashPassword('helloimpassword')  //You only run this when a user is creating a brand new account or changing their password.
+login('helloimpassword','$2b$12$dGcJ0aY0q3XBiUJ2jNK07ufEDmdna/1AaNszMyKofKbT1kxrlD0iq')
