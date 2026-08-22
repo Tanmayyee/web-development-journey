8cb4153bc8d522e6adee448b06f1169c34600c8b
Tanmay Johri <jo.tanmay7@gmail.com>
2026-08-22 12:31:36 +0530

    feat: add post routes

--- 08-auth-demo/AuthDemo/index.js
+++ 08-auth-demo/AuthDemo/index.js
@@ -2,13 +2,20 @@ import express from 'express';
 const app=express();
 import path from 'path'
 import User from './models/user.js';
+import mongoose from 'mongoose';
 
 
 app.set('view engine','ejs');
 app.set('views', path.join(import.meta.dirname,"/views"));
 
+app.use(express.urlencoded({extended:true}));
+
 app.get('/register',(req,res)=>{
     res.render('register')
+})
+
+app.post('/register',(req,res)=>{
+    res.send(req.body)
 })
 
 app.get('/secret',(req,res)=>{
