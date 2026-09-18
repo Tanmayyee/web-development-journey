543911ca4aaa123c6d0376af50826b31b6f507cc
Tanmay Johri <jo.tanmay7@gmail.com>
2026-09-18 20:37:00 +0530

    feat: add ColorBox exercise

---
+++ 09-React/05-Colorbox-exercise/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import ColorBox from './ColorBox'
+
+function App() {
+  return (
+    <>
+    <ColorBox colors={["red","blue","green","indigo","pink","black","white","yellow","orange",
+                  "purple","brown","olive","silver","aqua","tomato","violet"]}/>
+    </>
+  )
+}
+
+export default App
---
+++ 09-React/05-Colorbox-exercise/src/ColorBox.jsx
@@ -0,0 +1,18 @@
+import {useState} from 'react';
+import './ColorBox.css'
+
+export default function ColorBox({colors}){
+    const randomArr= (arr)=>{
+        const random=Math.floor(Math.random()*arr.length)
+        return arr[random];
+    }
+    return(
+        <>
+        <div 
+        className='StyleColorBox'
+        style={{backgroundColor:randomArr(colors)}}
+        >   
+        </div>
+        </>
+    )
+}
